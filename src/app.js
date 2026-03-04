62d02babf4cc6c5ec253e3676e1cb61a3491ff12
Sidney Paucar <sidneypaucar2000@gmail.com>
2026-03-03 21:29:37 -0500

    feat: express skeleton + env/logging/errors

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.get("/health", (req,res) => {
+res.status(200).json({ ok: true });
+});
+module.exports = app;
