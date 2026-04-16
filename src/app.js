98734b151a76c79f28dcde0d3079dc279b8d69a0
SAIN <sain@Sidneys-Mac-mini.local>
2026-04-15 20:52:57 -0400

    wired pino into app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const pinoHttp = require("pino-http");
+const logger = require("./utils/logger");
 const requestId = require("./middlewares/requestId");
+
 const app = express();
 
 app.use(express.json());
+
 app.use(requestId);
 
+app.use(
+	pinoHttp({
+		logger,
+		genReqId: (req) => req.requestId,
+		customProps: (req) => ({
+			requestId; req.requestId,
+		}),
+	})
+);
+
+
 app.get("/health", (req, res) => {
+	req.log.info(
+		{
+			requestId: req.requestId,
+			method: req:method,
+			path: req.url,
+		},
+		"health check hit"
+	);
+
 	res.status(200).json({ ok: true });
 });
 
